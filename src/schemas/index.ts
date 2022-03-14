20ffe0d96c709d86765d2491f31d785044cc8e21
miaoxingrenya <mr_liuzg@163.com>
2022-03-14 19:14:21 +0800

    modified three files and added poylgon and Mumbai configurations by lzg

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,9 +1,15 @@
 // To help typescript find the type
-
+// --begin--2022.3.14--lzg
+// 新增polygon与mumbai
 import { mainSchemas } from './main/index';
 import { rinkebySchemas } from './rinkeby/index';
+import {mumbaiSchemas} from './mumbai/index';
+import {polygonMainSchemas} from './polygon/index';
 
 export const schemas = {
   rinkeby: rinkebySchemas,
   main: mainSchemas,
+  mumbai: mumbaiSchemas,
+  polygon: polygonMainSchemas,
 };
+// --end--2022.3.14--lzg
